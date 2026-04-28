a051c47f0d7c86688db96ca9407cd695a6f4409b
vishakhshetty11 <vishakhshetty11@gmail.com>
2026-04-28 21:50:25 +0530

    Prepare backend for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -2,12 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const customerRoutes = require("./routes/customers");
 const app = express();
+require("dotenv").config();
+
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/customers", customerRoutes);
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
