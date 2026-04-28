346e435c8dc3ef969db1aadafc6623bfde8b4fbf
vishakhshetty11 <vishakhshetty11@gmail.com>
2026-04-28 17:58:15 +0530

    Added basic navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
+import Navbar from "./components/Navbar";
+
 
 function App() {
 
   return (
-     <h1>Customer Management Dashboard</h1>
+     <>
+      <Navbar />
+    </>
   )
 }
 
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,5 @@
+function Navbar() {
+  return <div className="navbar">Customer Management Dashboard</div>;
+}
+
+export default Navbar;
