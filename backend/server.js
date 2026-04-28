3dd6c117ef8978a68085d112ffa776a545a7999b
vishakhshetty11 <vishakhshetty11@gmail.com>
2026-04-29 02:33:38 +0530

    Added cors URL

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://arali-coral.vercel.app"],
+    origin: ["http://localhost:5173", "customer-management-six.vercel.app"],
   }),
 );
 app.use(express.json());
